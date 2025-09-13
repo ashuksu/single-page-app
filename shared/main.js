334b98d0a5b536e6f2af89585cb98061588559c9
ashuksu <ashuksu@gmail.com>
2025-09-13 11:50:56 +0200

    add routing functionality to dynamically render pages based on user interaction

--- shared/main.js
+++ shared/main.js
@@ -3,4 +3,18 @@ import {FrontendPage} from "../pages/frontend-component.js";
 import {BackendPage} from "../pages/backend-component.js";
 import React from "./react.js";
 
+const routing = {
+    'main': MainPage,
+    'frontend': FrontendPage,
+    'backend': BackendPage
+}
+
+
+document.querySelectorAll('[data-target]').forEach(el => {
+    el.addEventListener('click', () => {
+        const PageComponent = routing[el.dataset.target];
+        React.render(PageComponent, document.getElementById("root"));
+    });
+});
+
 React.render(MainPage, document.getElementById("root"));
